ac9a2174a6b2b3eb55810ca55defecd86eb54053
April Polubiec <aprilpolubiec@gmail.com>
2023-10-10 18:56:32 +0100

    load filter options into global var

--- main.js
+++ main.js
@@ -3,5 +3,24 @@ import senators from "./data/senators.js";
 // condense JSON into only the elements we need
 
 document.addEventListener("DOMContentLoaded", async () => {
-  console.log(senators);
+  const FILTER_OPTIONS = loadFilterOptions();
+  // TODO: ISSUE13 - load into els
 });
+
+function loadFilterOptions() {
+  var filterOptions = {
+    party: new Set(),
+    state: new Set(),
+    rank: new Set(),
+    gender: new Set(),
+  };
+
+  senators.objects.forEach(senator => {
+    filterOptions["party"].add(senator.party);
+    filterOptions["state"].add(senator.state);
+    filterOptions["rank"].add(senator.senator_rank);
+    filterOptions["gender"].add(senator.person.gender);
+  })
+
+  return filterOptions;
+}
