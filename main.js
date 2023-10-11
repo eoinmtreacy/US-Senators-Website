ac019f888039456c2f7124135cd3a9698645ebb8
April Polubiec <aprilpolubiec@gmail.com>
2023-10-11 15:12:22 +0100

    add filterOptions param to filter()

--- main.js
+++ main.js
@@ -1,9 +1,57 @@
 import senators from "./data/senators.js";
 
+class FilterOptions {
+  constructor() {
+    this.rank = new Set();
+    this.gender = new Set();
+    this.state = new Set();
+    this.party = new Set();
+  }
+
+  addFilter(type, value) {
+    switch (type) {
+      case "rank":
+        this.rank.add(value);
+        break;
+      case "gender":
+        this.gender.add(value);
+        break;
+      case "party":
+        this.party.add(value);
+        break;
+      case "state":
+        this.state.add(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  removeFilter(type, value) {
+    switch (type) {
+      case "rank":
+        this.rank.delete(value);
+        break;
+      case "gender":
+        this.gender.delete(value);
+        break;
+      case "party":
+        this.party.delete(value);
+        break;
+      case "state":
+        this.state.delete(value);
+        break;
+      default:
+        break;
+    }
+  }
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
-  const FILTER_LIST = filter()
+  const FILTER_LIST = filter();
   const FILTER_OPTIONS = loadFilterOptions();
-  // TODO: ISSUE13 - load into els
+  const currentFilter = new FilterOptions();
+
 });
 
 function loadFilterOptions() {
@@ -14,32 +62,35 @@ function loadFilterOptions() {
     gender: new Set(),
   };
 
-  senators.objects.forEach(senator => {
+  senators.objects.forEach((senator) => {
     filterOptions["party"].add(senator.party);
     filterOptions["state"].add(senator.state);
     filterOptions["rank"].add(senator.senator_rank);
     filterOptions["gender"].add(senator.person.gender);
-  })
+  });
 
   return filterOptions;
 }
 
-// filter function will accept filterType variables
-// condense JSON into only the elements we need
-// assignment details missing from brightspace, variables are placeholder
-
-function filter(){
-  let output = []
-  senators.objects.forEach(senator => {
-    let item = new Object()
-    item.id = senator.person.bioguideid
-    item.firstname = senator.person.firstname
-    item.secondname = senator.person.lastname
-    item.party = senator.party
-    item.state = senator.state
-    item.rank = senator.senator_rank
-    item.gender = senator.person.gender_label
-    output.push(item)
-  })
-  return output
+export function filter(filterOptionsObj) {
+  let output = [];
+  senators.objects.forEach((senator) => {
+    if (
+      filterOptionsObj.rank.has(senator.senator_rank) ||
+      filterOptionsObj.gender.has(senator.person.gender_label) ||
+      filterOptionsObj.state.has(senator.state) ||
+      filterOptionsObj.party.has(senator.party)
+    ) {
+      let item = new Object();
+      item.id = senator.person.bioguideid;
+      item.firstname = senator.person.firstname;
+      item.secondname = senator.person.lastname;
+      item.party = senator.party;
+      item.state = senator.state;
+      item.rank = senator.senator_rank;
+      item.gender = senator.person.gender_label;
+      output.push(item);
+    }
+  });
+  return output;
 }
