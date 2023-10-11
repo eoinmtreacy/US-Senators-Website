e2588c59142311de0d84c3fffc7da0bddf6b29db
Eoin Treacy <eointreacy5@gmail.com>
2023-10-11 16:50:38 +0100

    added complete options to filter

--- main.js
+++ main.js
@@ -116,6 +116,12 @@ function filter(filterOptionsObj) {
       item.state = senator.state;
       item.rank = senator.senator_rank;
       item.gender = senator.person.gender_label;
+      item.office = senator.extra.office
+      item.dob = senator.person.birthday
+      item.startdate = senator.startdate
+      item.twitter = senator.person.twitterid
+      item.youtube = senator.person.youtubeid
+      item.website = senator.website
       output.push(item);
     }
   });
@@ -153,30 +159,4 @@ function drawFilters(filterOptions) {
 // Utility functions
 function capitalizeFirstLetter(str) {
   return str[0].toUpperCase() + str.slice(1);
-}
-
-function draw()
-{
-  let output = []
-  senators.objects.forEach(senator =>
-    {
-      let item = new Object()
-      item.id = senator.person.bioguideid
-      item.party = senator.party
-      item.state = senator.state
-      item.rank = senator.senator_rank
-      item.gender = senator.person.gender
-      item.firstname = senator.person.firstname
-      item.lastname = senator.person.lastname
-      item.office = senator.extra.office
-      item.dob = senator.person.birthday
-      item.startdate = senator.startdate
-      item.twitter = senator.person.twitterid
-      item.youtube = senator.person.youtubeid
-      item.website = senator.website
-    
-      output.push(item)
-    })
-
-    return output
 }
